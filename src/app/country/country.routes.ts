249db487088ec4688587e2ad856003b45927386a
JohnFreddyTutistar <johnfre.157@gmail.com>
2025-10-22 09:42:26 -0500

    Creando componentes basados en DaisyUI para creación de footer, menú superior e iconos

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { CountryLayoutComponent } from './layouts/CountryLayout/CountryLayout';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page';
+
+export const countryRoutes: Routes = [
+
+  {
+    path: '',
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent
+      },
+
+      {
+        path: '**',
+        redirectTo: 'by-capital'
+      }
+    ]
+  }
+
+];
+
+
+export default countryRoutes
---
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'by-capital-page',
+  imports: [],
+  templateUrl: './by-capital-page.html',
+})
+export class ByCapitalPageComponent { }
